e277506db008fd86aeb61fb2f97fdaa9c9844211
sun1968 <35023070+sun1968@users.noreply.github.com>
2018-06-07 17:08:28 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,90 @@
+// JavaScript Document
+
+$(function(){
+	 // 轮播图开始 
+     var left = $('.content_middle .btnLeft');//获取左点击
+     var right = $('.content_middle .btnRight');//获取右点击
+
+     var aLi = $('.content_middle ul li');
+     var iNow = 0;
+      // 左点击  
+     left.click(function(){
+         iNow--;
+          // 判断回流
+         if(iNow<0){
+              iNow=2;
+         }
+         aLi.eq(iNow).siblings().stop().animate({
+          opacity:0
+         
+        },800);
+        aLi.eq(iNow).stop().animate({
+          opacity:1
+           
+        },800);
+   
+     });
+       // 右点击切换
+       right.click(function(){
+         iNow++;
+         if(iNow>2){
+              iNow=0;
+         }
+         aLi.eq(iNow).siblings().stop().animate({
+          opacity:0
+         
+        },800);
+        aLi.eq(iNow).stop().animate({
+          opacity:1
+           
+        },800);
+   
+
+     });
+
+       //手动切换
+
+  // 封装函数体
+ function move1(){
+      aLi.eq(iNow).siblings().stop().animate({
+          opacity:0
+         
+        },800);
+        aLi.eq(iNow).stop().animate({
+          opacity:1
+        },800);
+
+ }
+ 
+ // 定个定时器的初始值
+
+function run2(){ 
+      iNow++;
+       if(iNow>5){
+              iNow=0;
+         }
+       move1();  
+}
+ 
+// 定时器
+ timer = setInterval(run2,800);
+  
+ 
+
+//当鼠标划入，停止轮播图切换
+  $(".content_middle").hover(function(){
+    clearInterval(timer);
+ },function(){
+    timer = setInterval(run2,800);
+ }) 
+})
+
+$(function () {
+	$(".ny_zblb1 ul li").click(function(){
+		var thisSpan=$(this);
+		$(".ny_zblb1 ul li ul").prev("a").removeClass("cur");
+		$("ul", this).prev("a").addClass("cur");
+		$(this).children("ul").slideDown("fast");
+		$(this).siblings().children("ul").slideUp("fast");
+	})
+});
